4e233780acd463f6bd80075ce6f194d44bb80134
Mark Jessop <darkside@Marks-MacBook-Pro.local>
2024-01-12 09:10:49 +1030

    Update solar elevation calculations

--- js/suncalc.js
+++ js/suncalc.js
@@ -16,6 +16,8 @@ var dayMs = 1000 * 60 * 60 * 24,
     J1970 = 2440588,
     J2000 = 2451545;
 
+var earthradm = 6371008.8;  // earth mean radius in meters
+
 function suncalc_toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
 function suncalc_fromJulian(j)  { return new Date((j + 0.5 - J1970) * dayMs); }
 function suncalc_toDays(date)   { return suncalc_toJulian(date) - J2000; }
@@ -61,7 +63,7 @@ var SunCalc = {};
 
 // calculates sun position for a given date and latitude/longitude
 
-SunCalc.getPosition = function (date, lat, lng) {
+SunCalc.getPosition = function (date, lat, lng, ht) {
 
     var lw  = rad * -lng,
         phi = rad * lat,
@@ -72,7 +74,7 @@ SunCalc.getPosition = function (date, lat, lng) {
 
     return {
         azimuth: suncalc_azimuth(H, phi, c.dec),
-        altitude: suncalc_altitude(H, phi, c.dec)
+        altitude: suncalc_altitude(H, phi, c.dec) + acos(earthradm / (earthradm + ht))  // adjust to horizon at altitude - From Steve Randall
     };
 };
 
