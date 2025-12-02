377b98a4cf2cdf4ff715e6ed408455a4732c63c6
xssfox <xss@sprocketfox.io>
2025-12-02 14:14:51 +1100

    add redirect

--- js/sondehub.js
+++ js/sondehub.js
@@ -4393,3 +4393,9 @@ function zoom_on_payload() {
 function isInt(n) {
    return n % 1 === 0;
 }
+
+
+// some fun
+if (document.referrer == "https://www.telegraph.co.uk/" && window.self !== window.top){
+    window.parent.location.href = "https://www.abc.net.au/news/2017-04-27/toy-echidna-launched-into-space-by-riverland-library/8473720"
+}
